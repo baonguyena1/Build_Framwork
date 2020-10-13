25e0be8d4453ec98f0035ab33f64458d79abb7f5
Bao Nguyen <baon@tech.est-rouge.com>
2020-10-13 15:10:22 +0700

    first commit

---
+++ FruitBasket/Fruit.swift
@@ -0,0 +1,14 @@
+//
+//  Fruit.swift
+//  FruitBasket
+//
+//  Created by Bao Nguyen on 2020/10/13.
+//
+
+import Foundation
+
+public enum Fruit: String {
+    case apple
+    case banana
+    case orange
+}
